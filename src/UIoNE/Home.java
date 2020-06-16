22382086cda062b0ce57eeca7d0b91280c78c022
Bantamelak <automate2121@gmail.com>
2020-06-15 22:56:52 -0400

    this is the first commit

---
+++ src/UIoNE/Home.java
@@ -0,0 +1,17 @@
+package UIoNE;
+
+public class Home {
+    public void Home(){
+
+    }
+
+    String name = "Bantamelak";
+    int age = 26;
+    String dateOfBirth = "march";
+
+
+    public static void main(String [] args){
+        System.out.println("Bantamelak");
+
+    }
+}
